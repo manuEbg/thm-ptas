6505fcf1b1bc2984c7c44aa9e5e46f54a6b5c332
Jan Hindges <janfhindges@gmail.com>
2023-07-20 23:37:15 +0200

    Add BFS iterator and parent relations for tree decomposition.

--- src/graph/tree_decomposition.rs
+++ src/graph/tree_decomposition.rs
@@ -1,6 +1,7 @@
 use crate::graph::approximated_td::ApproximatedTD;
-use arboretum_td::tree_decomposition::TreeDecomposition;
+use arboretum_td::tree_decomposition::{Bag, TreeDecomposition};
 use fxhash::FxHashSet;
+use std::collections::{HashMap, VecDeque};
 
 /// This function is used to create a tree decomposition on one of the rings
 /// in a DCEL data structure.
@@ -11,7 +12,7 @@ impl From<&ApproximatedTD<'_>> for TreeDecomposition {
         let mut result = TreeDecomposition {
             bags: vec![],
             root: None,
-            max_bag_size: faces
+            max_bag_size: faces // TODO: Remove and count.
                 .iter()
                 .map(|face| face.walk_face(dcel).len())
                 .fold(0, Ord::max),
@@ -23,6 +24,9 @@ impl From<&ApproximatedTD<'_>> for TreeDecomposition {
                 vertices.insert(dcel.arc(arc).src());
             }
             result.add_bag(vertices);
+            if result.bags.len() == 1 {
+                result.root = Some(0);
+            }
         }
 
         for i in 0..approx_td.num_bags() {
@@ -36,10 +40,82 @@ impl From<&ApproximatedTD<'_>> for TreeDecomposition {
     }
 }
 
+#[derive(Clone, Copy)]
+enum NodeParent {
+    Fake,
+    Real(usize),
+}
+
+// TODO: When the children are not needed, this struct can be replaced by the hash map itself.
+// @speed The hash map could be replaced with a parent matrix.
+struct NodeRelations {
+    parent: HashMap<usize, NodeParent>,
+    // children: HashMap<usize, Vec<usize>>,
+}
+
+impl NodeRelations {
+    pub fn new(td: &TreeDecomposition) -> Self {
+        let mut parent = HashMap::new();
+        if let Some(root) = td.root {
+            parent.insert(root, NodeParent::Fake);
+        }
+        Self {
+            parent,
+            // children: HashMap::new(),
+        }
+    }
+}
+
+struct BfsIter<'a> {
+    td: &'a TreeDecomposition,
+    queue: VecDeque<usize>,   // Bag IDs.
+    visited: Vec<bool>,       // @speed Use a bitset.
+    relations: NodeRelations, // Should be filled by the iterator.
+}
+
+impl<'a> BfsIter<'a> {
+    pub fn new(td: &'a TreeDecomposition) -> Self {
+        BfsIter {
+            td,
+            queue: VecDeque::from([td.root.unwrap()]),
+            visited: vec![false; td.bags.len()],
+            relations: NodeRelations::new(&td),
+        }
+    }
+}
+
+impl<'a> Iterator for BfsIter<'a> {
+    type Item = (&'a Bag, NodeParent);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let front = self.queue.pop_front();
+        if front.is_none() {
+            return None;
+        }
+
+        let v = front.unwrap();
+        self.visited[v] = true;
+        let bag = &self.td.bags[v];
+        bag.neighbors // Find all unvisited neighbors.
+            .iter()
+            .filter(|&&n| !self.visited[n])
+            .for_each(|&n| {
+                self.queue.push_back(n);
+                // Every unvisited node thats parent is not set must be a child.
+                if self.relations.parent.get(&n).is_none() {
+                    self.relations.parent.insert(n, NodeParent::Real(bag.id));
+                }
+            });
+
+        Some((bag, *self.relations.parent.get(&bag.id).unwrap()))
+    }
+}
+
 pub trait NiceTreeDecomposition {
     fn make_nice(&self) -> TreeDecomposition;
 }
 
+// TODO: Rework with the 3 cases.
 impl NiceTreeDecomposition for TreeDecomposition {
     fn make_nice(&self) -> TreeDecomposition {
         let mut result = self.clone();
@@ -63,3 +139,60 @@ impl NiceTreeDecomposition for TreeDecomposition {
         result
     }
 }
+
+use std::fs::File;
+use std::io::{Error, Write};
+fn td_write_to_dot(file: &mut File, td: &TreeDecomposition) -> Result<(), Error> {
+    writeln!(file, "graph td {{")?;
+
+    let iter = BfsIter::new(&td);
+    for (bag, parent) in iter {
+        writeln!(
+            file,
+            "\tB{} [label=\"{{{}}}\"];",
+            bag.id,
+            bag.vertex_set
+                .iter()
+                .map(|&a| a.to_string())
+                .collect::<Vec<String>>()
+                .join(", ")
+        )?;
+
+        match parent {
+            NodeParent::Fake => {}
+            NodeParent::Real(parent) => {
+                writeln!(file, "\tB{} -- B{};", parent, bag.id)?;
+            }
+        }
+    }
+
+    writeln!(file, "}}")?;
+
+    Ok(())
+}
+
+    use crate::read_graph_file_into_dcel;
+    use std::io::Error;
+        let dcel = read_graph_file_into_dcel("data/exp.graph").unwrap();
+        let dcel = read_graph_file_into_dcel("data/exp.graph").unwrap();
+    }
+
+    #[test]
+    pub fn test_dot() -> Result<(), Error> {
+        let mut td = TreeDecomposition {
+            bags: Vec::new(),
+            root: None,
+            max_bag_size: 2,
+        };
+
+        let ab = td.add_bag(FxHashSet::from_iter(vec![0, 1]));
+        let cd = td.add_bag(FxHashSet::from_iter(vec![2, 3]));
+        let ef = td.add_bag(FxHashSet::from_iter(vec![4, 5]));
+        td.add_edge(ab, cd);
+        td.add_edge(ab, ef);
+
+        let path = "td.dot";
+        let mut td_out = File::create(path)?;
+        td_write_to_dot(&mut td_out, &td)?;
+
+        Ok(())
